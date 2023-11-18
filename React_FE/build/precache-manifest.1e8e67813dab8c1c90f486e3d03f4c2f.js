9c986b9ecb3bfe81f6d5d25311a5d6198d3685a6
Chamali Vishmani <chamali106astro@gmail.com>
2023-11-18 12:45:02 +0530

    build make

---
+++ React_FE/build/precache-manifest.1e8e67813dab8c1c90f486e3d03f4c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f7f187abb169414613726f7df56cf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bef190ca9fa51e11dbe7",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "6fbe7ff36fe599eda1ff",
+    "url": "/static/js/2.3a217375.chunk.js"
+  },
+  {
+    "revision": "bc620bd9cc8893328a5f453e25589d03",
+    "url": "/static/js/2.3a217375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef190ca9fa51e11dbe7",
+    "url": "/static/js/main.2c1b29c2.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
